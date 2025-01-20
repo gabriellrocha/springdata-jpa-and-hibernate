97e0f5cb71db353e2a494ca741dc2f646aca5ac3
gabriel <gabriell_rsilva@outlook.com>
2025-01-20 00:29:44 -0300

    embeddedId for composite PK

--- src/main/java/com/gabrielcoding/jpa/models/Video.java
+++ src/main/java/com/gabrielcoding/jpa/models/Video.java
@@ -8,6 +8,7 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.Polymorphism;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
---
+++ src/main/java/com/gabrielcoding/jpa/models/embedded/Order.java
@@ -0,0 +1,25 @@
+package com.gabrielcoding.jpa.models.embedded;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "_order")
+public class Order implements Serializable {
+
+    @EmbeddedId
+    private OrderId orderId;
+
+    private String information;
+    private String otherField;
+}
